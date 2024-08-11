cb2c993758749ecde6ab9f15e5149d72e647ad56
rupinderjdn <82199133+rupinderjdn@users.noreply.github.com>
2024-08-11 21:13:37 +0530

    path changes and build chagnes for images

--- src/assets/Data/CategoriesData.js
+++ src/assets/Data/CategoriesData.js
@@ -4,25 +4,25 @@ export const categories = [
     {
         id: 1,
         name: 'Women Hoseiry',
-        image: './src/assets/images/clayBirds.jpg',
+        image: './public/images/clayBirds.jpg',
         component: CategoryCard,  
     },
     {
         id: 2,
         name: 'Women Hoseiry',
-        image: './src/assets/images/clayBirds.jpg',
+        image: './public/images/clayBirds.jpg',
         component: CategoryCard,  
     },
     {
         id: 3,
         name: 'Women Hoseiry',
-        image: './src/assets/images/clayBirds.jpg',
+        image: './public/images/clayBirds.jpg',
         component: CategoryCard,  
     },
     {
         id: 4,
         name: 'Women Hoseiry',
-        image: './src/assets/images/clayBirds.jpg',
+        image: './public/images/clayBirds.jpg',
         component: CategoryCard,  
     }
 ]
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,32 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: "/shoppingShotsClient/"
-})
+export default ({ mode }) => {
+  return defineConfig({
+    plugins: [
+      react(),
+      // ViteMinifyPlugin({}),
+      // compression({
+      //   algorithm: "brotliCompress",
+      // }),
+    ],
+    base: "/shoppingShotsClient/",
+    build: {
+      outDir: "buildFolder",
+      assetsDir: "assetsFolder",
+      assetsInlineLimit: 4096,
+      sourcemap: mode === "dev",
+      rollupOptions: {
+        // external: (id) => {
+        //   if (id.endsWith("html")) return true;
+        // }, // Exclude image file types
+
+        output: {
+          dir: "./public/deploy",
+          entryFileNames: "bundle.js",
+          assetFileNames: "assets/bundle[extname]",
+        },
+      },
+    },
+  });
+};
