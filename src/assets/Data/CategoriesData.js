ea38624b413d831a56693b54ec912408dd65df9b
rupinderjdn <82199133+rupinderjdn@users.noreply.github.com>
2024-08-11 20:45:05 +0530

    images path changed

--- src/assets/Data/CategoriesData.js
+++ src/assets/Data/CategoriesData.js
@@ -4,25 +4,25 @@ export const categories = [
     {
         id: 1,
         name: 'Women Hoseiry',
-        image: '/shoppingShotsClient/src/assets/images/clayBirds.jpg',
+        image: './src/assets/images/clayBirds.jpg',
         component: CategoryCard,  
     },
     {
         id: 2,
         name: 'Women Hoseiry',
-        image: '/shoppingShotsClient/src/assets/images/clayBirds.jpg',
+        image: './src/assets/images/clayBirds.jpg',
         component: CategoryCard,  
     },
     {
         id: 3,
         name: 'Women Hoseiry',
-        image: '/shoppingShotsClient/src/assets/images/clayBirds.jpg',
+        image: './src/assets/images/clayBirds.jpg',
         component: CategoryCard,  
     },
     {
         id: 4,
         name: 'Women Hoseiry',
-        image: '/shoppingShotsClient/src/assets/images/clayBirds.jpg',
+        image: './src/assets/images/clayBirds.jpg',
         component: CategoryCard,  
     }
 ]
