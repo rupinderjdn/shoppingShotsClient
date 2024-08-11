86a28f97cce06b837d7141f1488aa977d6293f2f
rupinderjdn <82199133+rupinderjdn@users.noreply.github.com>
2024-08-11 21:30:02 +0530

    path changes back to public

--- src/assets/Data/CategoriesData.js
+++ src/assets/Data/CategoriesData.js
@@ -4,25 +4,25 @@ export const categories = [
     {
         id: 1,
         name: 'Women Hoseiry',
-        image: './publicDeploy/images/clayBirds.jpg',
+        image: './images/clayBirds.jpg',
         component: CategoryCard,  
     },
     {
         id: 2,
         name: 'Women Hoseiry',
-        image: './publicDeploy/images/clayBirds.jpg',
+        image: './images/clayBirds.jpg',
         component: CategoryCard,  
     },
     {
         id: 3,
         name: 'Women Hoseiry',
-        image: './publicDeploy/images/clayBirds.jpg',
+        image: './images/clayBirds.jpg',
         component: CategoryCard,  
     },
     {
         id: 4,
         name: 'Women Hoseiry',
-        image: './publicDeploy/images/clayBirds.jpg',
+        image: './images/clayBirds.jpg',
         component: CategoryCard,  
     }
 ]
